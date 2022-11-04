e85686fc6aa126a8d5c57108d3f9482633038c2b
Kang Jinhee <20wlsgml@gmail.com>
2022-11-04 23:27:47 +0900

    feat: Post 생성 Dto 수정
    1. 비밀번호가 비어있을 때 에러메세지 추가

--- src/boards/dto/create-boards.dto.ts
+++ src/boards/dto/create-boards.dto.ts
@@ -7,6 +7,6 @@ export class CreateBoardsDto {
   @MaxLength(200, { message: '본문은 200자 내외로 작성해주세요.' })
   content: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
   password: string;
 }
